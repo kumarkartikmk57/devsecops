6923683d797ec2829430c17884ec59d887e8c7c6
Kartik Kumar <kumarkartikmk57@gmail.com>
2023-10-01 06:13:58 +0000

    integration-test.sh edited online with Bitbucket

--- integration-test.sh
+++ integration-test.sh
@@ -1,43 +1,3 @@
-
-Dashboard
-Issues
-Pull Requests
-Milestones
-Explore
-Notifications
- Create…
-bob Profile and Settings…
-bob
-/
-kubernetes-devops-security
-Unwatch
-1
-Star
-0
-Fork
-0
-Code
-Issues
-Pull Requests
-Projects
-Releases
-Wiki
-Activity
-Settings
-devsecops app
-Manage Topics
- 2 Commits
- 1 Branch
- 0 Tags
- 327 KiB
- Branch: master 
-kubernetes-devops-security/integration-test.sh
-37 lines
-801 B
-Raw
-Permalink
-Blame
-History
 #!/bin/bash
 #integration-test.sh
 
@@ -74,4 +34,3 @@ else
         echo "The Service does not have a NodePort"
         exit 1;
 fi;
-Powered by Gitea Version: 1.14.1 Page: 32ms Template: 2ms Licenses API Website Go1.16.3
